bfaf059dfce9098abca212aeb704345fe1cece9b
Carlos Monroy Fernández <cmonroy@boardfy.com>
2019-03-11 11:28:11 +0100

    Fixed parsing disallowstatements containing disallow in url

--- src/RobotsTxt.php
+++ src/RobotsTxt.php
@@ -114,7 +114,7 @@ class RobotsTxt
 
     protected function parseDisallow(string $line): string
     {
-        return trim(str_replace('disallow', '', strtolower(trim($line))), ': ');
+        return trim(preg_replace('~disallow: ?~', '', strtolower(($line))));
     }
 
     protected function concernsDirectory(string $path): bool
